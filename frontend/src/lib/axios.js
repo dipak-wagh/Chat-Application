eeffc5989758d7310c81f7321de49a161136a157
Dipak Wagh <dipakwagh0502@gmail.com>
2026-03-24 13:50:34 +0530

    signup implemented on frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.mode === "development" ? "http://localhost:3000/api" : "/api",
+    baseURL: "http://localhost:3000/api",  // ✅ force correct backend
     withCredentials: true,
 });
